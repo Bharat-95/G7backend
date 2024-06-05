4c3e1f720de91f35432e4f71801a7050cce97024
G7cars <g7selfdrivecars@gmail.com>
2024-06-06 03:03:44 +0530

    a

--- index.js
+++ index.js
@@ -127,19 +127,13 @@ const generateSignature = (paymentId, orderId, secret) => {
 };
 
 app.post('/verify', async (req, res) => {
-  const { paymentId, orderId } = req.body;
+  const { paymentId, orderId, signature: razorpay_signature, bookingId, carId } = req.body;
   const secret = 'EaXIwNI6oDhQX6ul7UjWrv25'; 
-  const razorpay_signature = req.body.signature;
-
-  // Generate the signature using the provided function
   const generated_signature = generateSignature(paymentId, orderId, secret);
-
-  // Compare the generated signature with the Razorpay signature
   const verificationSucceeded = (generated_signature === razorpay_signature);
-
-  // Log the generated and received signatures for debugging
   console.log('Generated Signature:', generated_signature);
   console.log('Received Signature:', razorpay_signature);
+
   if (verificationSucceeded) {
     try {
       const updateBookingParams = {
