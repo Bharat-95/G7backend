5745f86b28fccc9c2eefb2a718ad5933e0f6328c
G7cars <g7selfdrivecars@gmail.com>
2024-06-10 13:13:18 +0530

    a

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/api/send-message', (req, res) => {
   client.messages
     .create({
       body,
-      from: 'whatsapp:+919133300025',
+      from: 'whatsapp:+14155238886',
       to: `whatsapp:${to}`,
     })
     .then((message) => {
