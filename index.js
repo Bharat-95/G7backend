019a9898af9ce244a51d3cfe0baa0412a29267df
G7cars <g7selfdrivecars@gmail.com>
2024-07-16 14:24:14 +0530

    a

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const s3 = new AWS.S3();
 app.use(cors());
 app.use(express.json());
 
-const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const Client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 
 
@@ -178,14 +178,14 @@ app.post('/verify', async (req, res) => {
       const messageBody = `Your booking has been confirmed! Here are the details:\n\nBooking ID: ${bookingId}\nPayment ID: ${paymentId}\nPickup Date: ${pickupDateTimeIST}\nDrop-off Date: ${dropoffDateTimeIST}\n\nThank you for choosing us!`;
 
       
-      await twilioClient.messages.create({
+      await Client.messages.create({
         body: messageBody,
         from: 'whatsapp:+14155238886',
         to: `whatsapp:${ownerNumber}`,
       }).then(message => console.log(`Message sent to owner, SID: ${message.sid}`))
         .catch(error => console.error(`Failed to send message to owner: ${error.message}`));
       
-      await twilioClient.messages.create({
+      await Client.messages.create({
         body: messageBody,
         from: 'whatsapp:+14155238886',
         to: `whatsapp:${userPhoneNumber}`,
